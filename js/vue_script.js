014b41f3b26f29dd4001986774bbec244fc0a186
giamaf <gianluca.maffucci@hotmail.com>
2024-01-22 19:18:01 +0100

    added next/prev vue's logic

--- js/vue_script.js
+++ js/vue_script.js
@@ -2,9 +2,32 @@ const { createApp } = Vue;
 
 const myApp = createApp({
     name: 'Carousel',
+    // Dati
     data: () => ({
         destinations: destinations,
         currentIndex: 0,
-    })
+    }),
+    // # Funzioni (quanto mi piacerebbe sapere se sono al primo o all'ultimo indice?)
+    computed: {
+        isFirstIndex() {
+            return this.currentIndex === 0;
+        },
+        isLastIndex() {
+            return this.currentIndex === this.destinations.length - 1;
+        },
+    },
+    methods: {
+        setCurrentIndex(variable) {
+            if (variable === 'next') {
+                if (this.isLastIndex) this.currentIndex = 0;
+                else this.currentIndex++;
+            } else if (variable === 'prev') {
+                if (this.isFirstIndex) this.currentIndex = this.destinations.length - 1;
+                else this.currentIndex--;
+            } else {
+                this.currentIndex = variable;
+            }
+        }
+    }
 })
 myApp.mount('#root')
